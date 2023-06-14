b0818a5398884269d9c98aecd026a5d6abbf6fe1
DON JEFFREY <95631932+DONJEFFREY720@users.noreply.github.com>
2023-06-14 18:25:10 +0530

    js

--- main.js
+++ main.js
@@ -6,19 +6,33 @@ function setup(){
      poseNet = ml5.poseNet(video,modelLoaded)
      poseNet.on("pose",gotResults)
 }
-
-function modelLoaded(){
-     console.log("MODEL LOADED!")
-}
-
-function draw(){
-     background("grey")
-     
-}
+noseX = 0
+noseY = 0
+leftWrist = 0
+difference = 0
+rightWrist = 0
 
 function gotResults(results){
      if(results.length>0){
           console.log(results)
+          noseX = results[0].pose.nose.x
+          noseY = results[0].pose.nose.y
+          console.log("NOSE X :"+noseX+
+          "NOSE Y : "+noseY)
+
+          leftWrist = results[0].pose.leftWrist.x
+          rightWrist = results[0].pose.rightWrist.x
+          difference = floor(leftWrist-rightWrist)
      }
+}
+
+function modelLoaded(){
+     console.log("MODEL READY!")
+}
+
+function draw(){
+     background("grey")
+     fill("yellow")
+     square(noseX,noseY,difference)
 }
 
